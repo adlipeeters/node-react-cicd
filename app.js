5c67d1b17742923f11e43471beb4a271c02b28f4
adlipeeters <adlipeeters@gmail.com>
2023-08-26 20:41:09 +0300

    configure github actions

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import path from 'path'
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 
 app.get('/names', (req, res) => {
@@ -11,4 +14,4 @@ app.listen(5000, () => {
     console.log('Server is running on port 5000');
 });
 
- 
+
