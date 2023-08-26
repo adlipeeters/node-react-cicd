3558bb3a3a37abdc788a18262e63439ac2faa89c
adlipeeters <adlipeeters@gmail.com>
2023-08-26 18:13:52 +0300

    Create node and react app

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/names', (req, res) => {
+    res.send('Andreqq');
+});
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
+
+ 
